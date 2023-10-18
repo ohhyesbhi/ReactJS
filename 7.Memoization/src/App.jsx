fb9d8dd681a2400c587e3af326137fc9d91c6455
unknown <kamatabhishek14@gmail.com>
2023-10-18 12:47:35 +0530

    added another problem

--- 7.Memoization/src/App.jsx
+++ 7.Memoization/src/App.jsx
@@ -17,10 +17,19 @@ function App() {
 
   // Here you cans ee whenever we click on add or substract then you can see that child component is re-rendering again and again
   // even if there are no updates in props so for it not re-render again and again we have to do memoization
+
+  // now suppose you introduced the function render
+  // and now you can see again the same problem of child re-rendering again after we click on add button any it don't have any connection
+  // but still it is re-rendering that is because whenever our page starts re-rendering the functions are recreated again and now we can the render
+  // function will be recreated again so now for child component it thinks that in function something has been changed
+
+  const render = () =>{
+    // simple operations are done
+  }
           
  return(
   <>
-    <Todo todos={todos}/>
+    <Todo todos={todos} render={render}/>
 
     <button onClick={()=>setAdd(add+1)}>Add</button>      <span>{add}</span>
     <br/>
